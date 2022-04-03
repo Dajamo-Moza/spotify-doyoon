ecc58cd8b74957ddbfc52a171dcaf1b182d2dfb6
doyoonear <doyoonleee@gmail.com>
2022-04-03 17:51:36 +0900

    feature: Add HTMLWebpackplugin, modify output setting

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import _ from 'lodash';
 import './style.css';
 import ArrowIcon from '../public/assets/arrow.svg';
 import mockData from './mockData.json';
-import yaml from './sample.yaml';
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
   // Lodash, now imported by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.textContent = mockData.name;
   element.classList.add('hello');
 
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
   const myIcon = new Image();
   myIcon.src = ArrowIcon;
-
+  
+  element.appendChild(btn);
   element.appendChild(myIcon);
 
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const yaml = require('yamljs');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
