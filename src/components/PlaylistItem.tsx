8a53a2d6de02d92068d134d7f66d429311fbbea7
doyoonear <doyoonleee@gmail.com>
2022-04-24 20:32:33 +0900

    redesign: Add playlist hover, save style

--- src/components/PlaylistItem.tsx
+++ src/components/PlaylistItem.tsx
@@ -5,7 +5,7 @@ const PlaylistItem = ({ name }: { name: string }) => {
   return <StyledPlaylistItem>{name}</StyledPlaylistItem>;
 };
 
-const StyledPlaylistItem = styled.div`
+const StyledPlaylistItem = styled.li`
   width: 100%;
   display: flex;
   align-items: center;
@@ -13,6 +13,23 @@ const StyledPlaylistItem = styled.div`
   background-color: ${({ theme }) => theme.color.white};
   border: 0.1rem solid ${({ theme }) => theme.color.primary};
   height: 4rem;
+  cursor: pointer;
+
+  &:not(li:last-of-type) {
+    margin-bottom: ${({ theme }) => theme.gutter.size8};
+  }
+
+  &:hover {
+    background-color: #d3f2e3;
+
+    &:after {
+      position: fixed;
+      right: 5rem;
+      font-weight: 700;
+      color: ${({ theme }) => theme.color.primary};
+      content: 'SAVE';
+    }
+  }
 `;
 
 export default PlaylistItem;
--- src/components/PlaylistModal.tsx
+++ src/components/PlaylistModal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import PlaylistItem from '@/components/PlaylistItem';
 import { getAxiosData } from '@/utils/getData';
+import CloseIcon from '@/assets/close.svg';
 
 interface PlayList {
   id: string;
@@ -27,6 +28,9 @@ const PlaylistModal = () => {
   return (
     <StyledPlaylistModal>
       <Title>MY PLALISTS</Title>
+      <AbsoluteBox>
+        <CloseIcon />
+      </AbsoluteBox>
       {playList?.length > 0 && playList.map((item) => <PlaylistItem name={item.name} />)}
     </StyledPlaylistModal>
   );
@@ -36,11 +40,11 @@ const StyledPlaylistModal = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: fixed;
   z-index: 100;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  position: fixed;
   background-color: #3a3a3a;
   width: 40rem;
   min-height: 50rem;
@@ -52,7 +56,14 @@ const Title = styled.h2`
   font-size: ${({ theme }) => theme.font.size20};
   text-align: center;
   color: ${({ theme }) => theme.color.primary};
+  margin-top: ${({ theme }) => theme.gutter.size8};
   margin-bottom: ${({ theme }) => theme.gutter.size28};
+`;
+
+const AbsoluteBox = styled.div`
+  position: absolute;
+  top: 2rem;
+  right: 3rem;
 `;
 
 export default PlaylistModal;
