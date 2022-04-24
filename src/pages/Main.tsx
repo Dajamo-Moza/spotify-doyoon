19384720fd825657c2749c104e10ab02f894ade4
doyoonear <doyoonleee@gmail.com>
2022-04-24 20:17:58 +0900

    feature: Add modal style, modal get api

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from '@/components/Layout';
 import SideBar from '@/components/Sidebar';
 import AlbumList from '@/components/AlbumList';
+import PlaylistModal from '@/components/PlaylistModal';
 
 function Main() {
   return (
     <Layout>
       <SideBar />
       <AlbumList />
+      <PlaylistModal />
     </Layout>
   );
 }
