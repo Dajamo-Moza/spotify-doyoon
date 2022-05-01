09055f44fb090824d66900f89a5bfb2597a4b764
doyoonear <doyoonleee@gmail.com>
2022-05-01 23:15:08 +0900

    feature: Add background cover

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '@/components/Layout';
 import SideBar from '@/components/Sidebar';
 import AlbumList from '@/components/AlbumList';
 import PlaylistModal from '@/components/PlaylistModal';
+import { useRecoilValue } from 'recoil';
+import { isPlaylistModalOpenState } from '@/atoms/index';
 
 function Main() {
+  const isModalOpen = useRecoilValue<boolean>(isPlaylistModalOpenState);
+
   return (
-    <Layout>
-      <SideBar />
-      <AlbumList />
-      <PlaylistModal />
-    </Layout>
+    <>
+      <CoverBackground isModalOpen={isModalOpen} />
+      <Layout>
+        <SideBar />
+        <AlbumList />
+        <PlaylistModal />
+      </Layout>
+    </>
   );
 }
+
+const CoverBackground = styled.div<{ isModalOpen: boolean }>`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: black;
+  opacity: 0.3;
+`;
 
 export default Main;
